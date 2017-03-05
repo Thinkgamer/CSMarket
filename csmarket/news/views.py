fe6b6a1b5148595ad70a52c8d3f51c6d5d8746c2
thinkgamer <thinkgamer@163.com>
2017-03-06 00:34:46 +0800

    add base of head and foot

--- csmarket/news/views.py
+++ csmarket/news/views.py
@@ -6,8 +6,14 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 def all(request,newcate):
     #这里传进来的应该是分类的名字
-    #new_cate=2 代表是CSM动态  1 代表是创客故事
+    #new_cate=2 代表是CSM动态,  1 代表是创客故事
     cate_id=cate.objects.get(cate_name=newcate).id
+    if cate_id==1:
+        csmdongtai=False;chuangke=True
+    elif cate_id==2:
+        chuangke=False;csmdongtai=True
+    else:
+        pass
     new_list = News.objects.filter(new_cate=cate_id).order_by("-new_time")
     paginator = Paginator(new_list, 20)  # Show 20 contacts per page
     page = request.GET.get('page')
@@ -20,6 +26,9 @@ def all(request,newcate):
         # If page is out of range (e.g. 9999), deliver last page of results.
         all_new = paginator.page(paginator.num_pages)
     return render(request, 'news.html', {
+        'csmdongtai': csmdongtai,
+        'chuangke': chuangke,
+        'catename': newcate,
         'all_new': all_new,
         "len_list": range(1, paginator.num_pages+1),
     })
@@ -30,6 +39,13 @@ def one(request,newtitle):
         pass
     else:
         new=News.objects.get(new_title=newtitle)
+        if new.new_cate_id==1:
+            csmdongtai = False;chuangke = True
+        elif new.new_cate_id==2:
+            csmdongtai = True;chuangke = False
         return render_to_response('news_one.html',{
             'new': new,
+            'csmdongtai': csmdongtai,
+            'chuangke': chuangke,
+            'title': newtitle,
         })
