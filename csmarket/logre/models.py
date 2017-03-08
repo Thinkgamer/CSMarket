a3efe882fa756d24245d47b6ff4f3451aafe2e0c
thinkgamer <thinkgamer@163.com>
2017-03-08 23:34:23 +0800

    add index.html

--- csmarket/logre/models.py
+++ csmarket/logre/models.py
@@ -1,10 +1,7 @@
 #-*-coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.db.models.signals import post_save
-
 # Create your models here.
-
 
 class User(AbstractUser):
     # 手机号
